02d4fa7092a8af250360010e2450afd3f427d580
adeloing <adeloing@gmail.com>
2022-10-31 21:55:33 +0100

    ok

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -5,4 +5,22 @@ class HomeController < ApplicationController
         puts  params[:user]
         puts "$" * 60
     end
+    def home
+        @gossips = Gossip.all
+        puts "$" * 60
+        puts "index"
+        puts "$" * 60
+    end
+    def gossip_by_id
+        @gossip = Gossip.find(params[:id])
+        puts "$" * 60
+        puts "gossip_by_id"
+        puts "$" * 60
+    end
+    def user_by_id
+        @user = User.find(params[:id])
+        puts "$" * 60
+        puts "user_by_id"
+        puts "$" * 60
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
-  get '/home', to: 'home#home'
+  get '/', to: 'home#home'
   get '/welcome/:user', to: 'home#welcome'
+  get '/gossip_by_id/:id', to: 'home#gossip_by_id'
+  get '/user_by_id/:id', to: 'home#user_by_id'
   get 'static_pages/team'
   get 'static_pages/contact'
 end
